01300ece745c38b32ab89f3644c419b0cb4adc76
Edwin <eanieto.9305@gmail.com>
2021-05-03 22:21:06 -0500

    agregaciones

--- vista_general_unificada.sql
+++ vista_general_unificada.sql
@@ -1,21 +1,22 @@
+CREATE OR REPLACE VIEW vw_general_unificada AS
 SELECT
        A.departamento,
        año_hecho AS año,
-       SUM(A.cantidad) AS cantidad,
+       SUM(A.cantidad) AS cantidad_hechos_policia,
        SUM(B.prc_pobreza) AS prc_pobreza,
-       SUM(C.CAPTURA_CONTEO),
-       SUM(C.IMPUTACION_CONTEO),
-       SUM(C.CONDENA_CONTEO),
-       SUM(TOTAL_PROCESOS)
+       SUM(C.CAPTURA_CONTEO) total_capturados,
+       SUM(C.IMPUTACION_CONTEO) total_imputados,
+       SUM(C.CONDENA_CONTEO) total_condenados,
+       SUM(TOTAL_PROCESOS) total_procesos,
+       SUM(d."TOTAL")cantidad_migrantes_venezolanos
 FROM vw_delitos_policia as A
-INNER JOIN (
+LEFT JOIN (
 select
        UPPER(departamento) AS departamento,
        año,
-       null as cantidad,
        prc_pobreza+prc_pobreza_extrema prc_pobreza
 from tbl_pobreza) AS B ON A.departamento = B.departamento AND A.año_hecho = B.año
-INNER JOIN (
+LEFT JOIN (
 SELECT
 UPPER("DEPARTAMENTO") departamento,
 "ANIO_HECHO" año,
@@ -27,13 +28,94 @@ from depuracion_procesos_judiciales
 WHERE "ANIO_HECHO" NOT IN (2016,2020)
 GROUP BY 1,2
     ) AS c on c.departamento = a.departamento and c.año = a.año_hecho
+LEFT JOIN (
+    SELECT
+    anio,
+    UPPER("DEPARTAMENTO") departamento,
+    "TOTAL"
+    FROM migracion_venezolanos
+    ) AS d on d.departamento = a.departamento and d.anio = a.año_hecho
 GROUP BY 1,2
 ORDER BY 1;
 
 
-select
-"Año",
-SUM("Total")
-from "Entradas_de_extranjeros_a_Colombia"
 
+/*
+Limpieza de tabla de procesos judiciales
+homologacion de departamentos
+*/
+
+select distinct "DEPARTAMENTO" from depuracion_procesos_judiciales
+where "DEPARTAMENTO" = 'VALLE';
+
+
+UPDATE depuracion_procesos_judiciales
+SET"DEPARTAMENTO" = 'SAN ANDRÉS'
+WHERE "DEPARTAMENTO" = 'Archipiélago de San Andrés, Providencia y Santa Catalina';
+
+UPDATE depuracion_procesos_judiciales
+SET"DEPARTAMENTO" = 'VALLE'
+WHERE "DEPARTAMENTO" = 'Valle del Cauca';
+
+
+UPDATE depuracion_procesos_judiciales
+SET"DEPARTAMENTO" = 'GUAJIRA'
+WHERE "DEPARTAMENTO" = 'La Guajira';
+
+UPDATE depuracion_procesos_judiciales
+SET"DEPARTAMENTO" = 'BOYACÁ'
+WHERE "DEPARTAMENTO" = 'Boyaca';
+
+
+/*
+Limpieza de tabla de tbl_pobreza
+homologacion de departamentos
+*/
+select
+       distinct UPPER(departamento)
+from sc_fuentes_data.tbl_pobreza;
+
+UPDATE sc_fuentes_data.tbl_pobreza
+SET departamento = 'VALLE'
+WHERE UPPER(departamento) = 'VALLE DEL CAUCA';
+
+
+UPDATE sc_fuentes_data.tbl_pobreza
+SET departamento = 'GUAJIRA'
+WHERE UPPER(departamento) = 'LA GUAJIRA';
+
+
+UPDATE sc_fuentes_data.tbl_pobreza
+SET departamento = 'PUTUMAYO'
+WHERE UPPER(departamento) = 'LA GUAJIRA';
+
+
+/*
+Limpieza de tabla de migracion_venezolanos
+homologacion de departamentos
+*/
+
+SELECT
+DISTINCT "DEPARTAMENTO"
+FROM migracion_venezolanos;
+
+UPDATE sc_fuentes_data.migracion_venezolanos
+SET "DEPARTAMENTO" = 'BOLÍVAR'
+WHERE "DEPARTAMENTO" = 'Bolivar';
+
+UPDATE sc_fuentes_data.migracion_venezolanos
+SET "DEPARTAMENTO" = 'GUAJIRA'
+WHERE "DEPARTAMENTO" = 'La Guajira';
+
+UPDATE sc_fuentes_data.migracion_venezolanos
+SET "DEPARTAMENTO" = 'SAN ANDRÉS'
+WHERE "DEPARTAMENTO" = 'San Andrés y Providencia';
+
+UPDATE sc_fuentes_data.migracion_venezolanos
+SET "DEPARTAMENTO" = 'SAN ANDRÉS'
+WHERE "DEPARTAMENTO" = 'San Andrés Islas';
+
+UPDATE sc_fuentes_data.migracion_venezolanos
+SET "DEPARTAMENTO" = 'VALLE'
+WHERE "DEPARTAMENTO" = 'Valle del Cauca';
 
